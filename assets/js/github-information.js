098f53494f33efc41dff5e8a7b1efd1233e3183d
SeanHub2021 <shbristol12@gmail.com>
2024-01-15 19:00:05 +0000

    add repoInformation function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,13 +1,13 @@
-function userInformationHTML(user){
+function userInformationHTML(user) {
     return `
-        <h2>${user.name}</h2>
+        <h2>${user.name}
             <span class="small-name">
                 (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
             <div class="gh-avatar">
-                <a href="${user.html_url} target="_blank">
+                <a href="${user.html_url}" target="_blank">
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
@@ -15,6 +15,28 @@ function userInformationHTML(user){
         </div>`;
 }
 
+
+
+function repoInformationHTML(repos) {//pass the function the ITEM returned from the gitHub API!
+    if (repos.length == 0) {//data is returned as an array, so a 0 length = no repo data
+        return `<div class="repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {//the .map METHOD iterates through like "forEach" but returns an array with the results of the function!
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;//target="_blank" opens the link a new window!
+    });
+
+    return `<div class="repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`; //call the array var we created earlier in the function "listItemsHTML" and JOIN everything with a new line. This provides all the items within the var as an unordered list.
+}
 
 function fetchGitHubInformation(event) {
 
@@ -29,12 +51,15 @@ function fetchGitHubInformation(event) {
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-    $.when(//jquery promise
-        $.getJSON(`https://api.github.com/users/${username}`)
+    $.when(//jquery promise!
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
